f9fe6870731dcecf5cb32434df4d0c942b766e4c
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2021-11-23 23:51:30 +0000

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Conversor de texto a número" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication4._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <script src="https://unpkg.com/tailwindcss-jit-cdn"></script>
+    <div class="jumbotron flex flex-row justify-center text-5xl">
+        <h2>Conversor de texto a número</h2>
+    </div>
+
+    <div class="row">
+        <p class="italic text-center">Bienvenido a esta página web. Por favor, inserte el texto con números que desea convertir en el siguiente recuadro (el máximo número es 18446744073709551615):</p>
+        <div class="flex flex-col items-center mt-10">
+            <asp:TextBox ID="TextBox1" runat="server" CssClass="rounded-md w-1/3"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Enviar" CssClass="mt-3 w-1/3 rounded-md" Visible="True" OnClick="Button1_Click" UseSubmitBehavior="False" />
+            <asp:Label ID="Label1" runat="server" CssClass="mt-5"></asp:Label>
+        </div>
+        
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Text;
+
+namespace WebApplication4
+{
+    public partial class _Default : Page
+    {
+        static Dictionary<string, string> numberSet = new Dictionary<string, string>();
+        private static string prevNumberInserted = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string numberFilePath = @"C:\Users\Alexandru\Desktop\a.txt";
+            try
+            {
+                if (File.Exists(numberFilePath))
+                {
+                    using (StreamReader sr = new StreamReader(numberFilePath))
+                    {
+                        while (sr.Peek() >= 0)
+                        {
+                            string[] vs = sr.ReadLine().Split(';');
+                            numberSet.Add(vs[0], vs[1]);
+                        }
+                        sr.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] vs = TextBox1.Text.Split(' ');
+                string finalResult = "";
+                foreach (string number in vs)
+                {
+                    if (numberSet.ContainsKey(number))
+                    {
+                        if (finalResult == "")
+                        {
+                            prevNumberInserted = numberSet[number];
+                            finalResult = numberSet[number];
+                        }
+                        else finalResult = JoinNumbersInString(finalResult, numberSet[number]);
+                    }
+                    else
+                    {
+                        if (number == "y") continue;
+                        throw new Exception("Error: Número inválido");
+                    }
+                }
+                // Separate each three from right to left.
+                finalResult = separateNumbers(finalResult);
+                Label1.Text = "Resultado: " + "\"" + finalResult.Trim() + "\"";
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+            }
+            for (int i = 0; i < Application.AllKeys.Length; i++)
+            {
+                Label1.Text += Application[i];
+            }
+        }
+        /**
+         * 
+         * <summary>Method for separating strings each three characters from right to left.</summary>
+         * <param name="str">The string to be separated.</param>
+         * <returns>The separated string.</returns>
+         * 
+         */
+        private string separateNumbers(string str)
+        {
+            string reversedString = reverseString(str);
+            for (int i = 0; i < reversedString.Length; i++)
+            {
+                if (i%4 == 0)
+                {
+                    reversedString = reversedString.Substring(0, i) + " " + reversedString.Substring(i);
+                }
+            }
+            return reverseString(reversedString);        
+        }
+        /**
+         * <summary>Method to reverse a string easily.</summary>
+         * <param name="str">String to be reversed.</param>
+         * <returns>The reversed string</returns>
+         * 
+         */
+        private string reverseString(string str)
+        {
+            char[] vs = str.ToCharArray();
+            Array.Reverse(vs);
+            return new string(vs);
+        }
+        /**
+         * 
+         * <summary>Shifts a character to the left in the number (eg., if we wanted to shift "40" two times in "20040", the result would be "24000").</summary>
+         * <param name="c">Character to be shifted.</param>
+         * <param name="s">String where to shift the character.</param>
+         * <param name="nShifts">Amount of positions to be shifted.</param>
+         * <returns>The param s with the shifted character.</returns>
+         * 
+         */
+        private string shiftCharToLeftOfNumber(char c, string s, int nShifts)
+        {
+            string result = s;
+            StringBuilder sb = new StringBuilder(result);
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                if (s[i] == c && s[i] != '0')
+                {
+                    if (i-nShifts <= 0)
+                    {
+                        return result += "0";
+                    } else
+                    {
+                        sb[i - nShifts] = c;
+                        sb[i] = '0';
+                    }
+                    break;
+                }
+            }
+            result = sb.ToString();
+            return result;
+        }
+        /**
+         * 
+         * <summary>Joins two number strings into one.</summary>
+         * <param name="num1">Number to be joined.</param>
+         * <param name="num2">Number to join.</param>
+         * <returns>The joined numbers.</returns>
+         * 
+        **/
+        private string JoinNumbersInString(string num1, string num2)
+        {
+            string result = "";
+            if (num1 == "y" || num2 == "y") return "";
+            if (prevNumberInserted.Length > num2.Length)
+            {
+                result = num1;
+                StringBuilder sb = new StringBuilder(result);
+                int posNum1 = result.Length - 1;
+                int posNum2 = num2.Length - 1;
+                while (posNum2 >= 0)
+                {
+                    sb[posNum1] = num2[posNum2];
+                    posNum1--;
+                    posNum2--;
+                }
+                result = sb.ToString();
+            } else
+            {
+                result = num1;
+                if (num1.Length > num2.Length)
+                {
+                    string initialString = result.Substring(result.Length - 3);
+                    for (int j = 0; j < initialString.Length; j++)
+                    {
+                        result = shiftCharToLeftOfNumber(initialString[j], result, num2.Length-1);
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < num2.Length; i++)
+                    {
+                        if (num1.Length < num2.Length) result += "0";
+                    }
+                }
+                                       
+            }
+            prevNumberInserted = num2;
+            return result;
+        }
+        /**
+         * 
+         * <summary>Method to get the list of words off a file.</summary>
+         * <returns>The list containing the words.</returns>
+         * 
+         */
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true)]
+        public static List<string> GetWords()
+        {
+            List<string> result = new List<string>();
+            foreach (KeyValuePair<string, string> keyValuePair in numberSet) {
+                result.Add(keyValuePair.Key);
+            }
+            return result;
+        }
+    }
+}
